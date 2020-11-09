b941f2a38559eda00e4629c1b840323534b59deb
naoya sakuma <4869ns@gmail.com>
2020-11-09 16:07:37 +0900

    アクション、ビューの共通化

--- app/controllers/mussitations_controller.rb
+++ app/controllers/mussitations_controller.rb
@@ -1,4 +1,5 @@
 class MussitationsController < ApplicationController
+before_action :set_mussitation, only: [:edit, :update]
   def index
     @mussitation = Mussitation.all
   end
@@ -14,10 +15,10 @@ class MussitationsController < ApplicationController
     end
   end
   def edit
-    @mussitation = Mussitation.find(params[:id])
+    #@mussitation = Mussitation.find(params[:id])
   end
   def update
-    @mussitation = Mussitation.find(params[:id])
+    #@mussitation = Mussitation.find(params[:id])
     if @mussitation.update(mussitation_params)
       redirect_to mussitations_path, notice: "ブログを編集しました！"
     else
@@ -27,5 +28,8 @@ class MussitationsController < ApplicationController
   private
   def mussitation_params
     params.require(:mussitation).permit(:content)
+  end
+  def set_mussitation
+    @mussitation = Mussitation.find(params[:id])
   end
 end
