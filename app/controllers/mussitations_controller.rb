d8f19437c722030654e807508445e057954de600
naoya sakuma <4869ns@gmail.com>
2020-11-09 14:28:42 +0900

    投稿画面にリダイレクト処理を追加

--- app/controllers/mussitations_controller.rb
+++ app/controllers/mussitations_controller.rb
@@ -6,5 +6,6 @@ class MussitationsController < ApplicationController
   end
   def create
     Mussitation.create(content: params[:mussitation][:content])
+    redirect_to new_mussitation_path
   end
 end
