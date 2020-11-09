e8de0544ecb3015e1e1f4c79e6ebadab37cb5c1f
naoya sakuma <4869ns@gmail.com>
2020-11-09 14:44:36 +0900

    投稿一覧画面を作成

--- app/controllers/mussitations_controller.rb
+++ app/controllers/mussitations_controller.rb
@@ -1,11 +1,17 @@
 class MussitationsController < ApplicationController
   def index
+    @mussitation = Mussitation.all
   end
   def new
     @mussitation = Mussitation.new
   end
   def create
-    Mussitation.create(content: params[:mussitation][:content])
+    Mussitation.create(mussitation_params)
     redirect_to new_mussitation_path
+  end
+
+  private
+  def mussitation_params
+    params.require(:mussitation).permit(:content)
   end
 end
